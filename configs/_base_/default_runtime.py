02e731de3e5e18b389e0539a016661be84150606
Samarth Ramesh <64597521+samarthramesh@users.noreply.github.com>
2021-05-15 11:22:11 +0530

    Update default_runtime.py

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -9,6 +9,6 @@ log_config = dict(
 # yapf:enable
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = None
+load_from = '/content/drive/MyDrive/Colab Data/RASEET Data/Models/db_cascade_mask_rcnn_x101_32x4d_fpn_dconv_c3-c5_1x_coco-e75f90c8.pth'
 resume_from = None
 workflow = [('train', 1)]
